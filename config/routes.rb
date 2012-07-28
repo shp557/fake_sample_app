d0492dea58bf39ac42b22d70adc13eed758629db
Sachin Patel <sachb52@gmail.com>
2012-07-27 21:51:33 -0400

    Some CSS Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/about"
-  get "static_pages/help"
+  root to: 'static_pages#home'
+
+  match '/home', to: 'static_pages#home'
+  match '/about', to: 'static_pages#about'
+  match '/help', to: 'static_pages#help'
+  match '/contact', to: 'static_pages#contact'
+  match '/', to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
