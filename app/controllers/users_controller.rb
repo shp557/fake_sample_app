65efa756f07119381d75dc9acc51d6813145ac5f
Sachin Patel <sachb52@gmail.com>
2012-07-29 16:17:16 -0400

    Final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -7,7 +10,7 @@ class UsersController < ApplicationController
   	@user = User.new(params[:user])
   	if @user.save
   		sign_in @user
-  		flash[:success] = "Welcome to the Sample App!"
+  		flash[:success] = "Welcome to HarvardPulse!"
   		redirect_to @user
   	else	
   		render 'new'
@@ -17,4 +20,31 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
