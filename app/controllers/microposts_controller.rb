2e674951da47462ee3e0d0d2695fe81b9f95f58b
Sachin Patel <sachb52@gmail.com>
2012-08-02 12:00:01 -0400

    Some changes

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,7 @@ class MicropostsController < ApplicationController
   before_filter :signed_in_user, only: [:new, :create, :destroy]
   before_filter :correct_user,   only: :destroy
   
+
   def new
   	@micropost = Micropost.new
   end
@@ -27,8 +28,12 @@ class MicropostsController < ApplicationController
 
   private
 
+    def update
+      
+    end
+
     def correct_user
-      @micropost = current_user.microposts.find_by_id(params[:id])
+      micropost = current_user.microposts.find_by_id(params[:id])
       redirect_to root_path if @micropost.nil?
     end
 end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :date, :event, :start_time, :end_time, 
   				  :description, :location, :time_start
-  
+  before_save :update
   belongs_to :user 
 
   validates :user_id, presence: true
@@ -11,5 +11,15 @@ class Micropost < ActiveRecord::Base
   validates :start_time, presence: true
   validates :end_time, presence: true
 
-  default_scope order: 'microposts.created_at DESC'
+  default_scope order: 'microposts.start_time DESC'
+
+  private
+
+  	def update
+  	  @the_date = self.date
+  	  @the_time = self.start_time
+  	  @the_datetime = DateTime.new(@the_date.year, @the_date.month, @the_date.day,
+  	  	                           @the_time.hour, @the_time.min, @the_time.sec)
+      self.start_time = @the_datetime
+    end
 end
